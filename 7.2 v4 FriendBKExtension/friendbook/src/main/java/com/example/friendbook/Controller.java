efba9ee7e9e45a3048ab0c586f40a12ac041812b
ansonc314 <117306795+ansonc314@users.noreply.github.com>
2023-01-01 16:31:12 +0800

    7.2 v4 need comments

--- 7.2 v4 FriendBKExtension/friendbook/src/main/java/com/example/friendbook/Controller.java
+++ 7.2 v4 FriendBKExtension/friendbook/src/main/java/com/example/friendbook/Controller.java
@@ -9,12 +9,36 @@ import java.io.IOException;
 public class Controller {
     int show_index_home;   // index of friend list for the friend to be displayed/deleted
 
+    public TextArea tarea_instruction;
     public TextField text_group, text_addGetName, text_addGetPhone, text_addGetEmail;
     public Label lbl_ListShowName_home, lbl_ListShowPhone_home, lbl_ListShowEmail_home;
     public ListView<Friend> friendList_home = new ListView<Friend>();
     public Button button_delete_home;
-    public ChoiceBox  cbox_add_choice;
 
+
+    @FXML
+    public void initialize() {
+        String str =
+                  "1) To save a list to a file\n" +
+                          "\ta) Choose the group name  \n" +
+                          "\tb) Click the save button   \n" +
+                          "\tc) The current friend list will save to a file, where the file name is the group name. e.g., if group name is home, the file is called home.txt  \n" +
+                          "2) To load the list from a file\n" +
+                          "\ta) Choose the group name  \n" +
+                          "\tb) Click the load button   \n" +
+                          "\tc) The list will be loaded using the corresponding data file. e.g., if group name is home, the file is called home.txt  \n" +
+                          "3) To add a friend to a list\n" +
+                          "\ta) Choose the group name  \n" +
+                          "\tb) Enter the friend's details and press add button.   \n" +
+                          "\tc) The friend will be added to the current list. Remember to save the list after all editing. \n" +
+                          "4) To clear a list\n" +
+                          "\ta) Use the clear button  \n" +
+                          "   ";
+        tarea_instruction.setText(str);
+    }
+    public void clearFriend(){
+        friendList_home.getItems().clear();
+    }
     public void addFriend(){
         /*  Requires: Strings (name, phone and email) from text fields
             Modifies: friendList
--- 7.2 v4 FriendBKExtension/friendbook/src/main/java/com/example/friendbook/Main.java
+++ 7.2 v4 FriendBKExtension/friendbook/src/main/java/com/example/friendbook/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 import java.io.*;
 
@@ -17,6 +18,7 @@ import javafx.collections.*;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Friendbook.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Friendbook");
