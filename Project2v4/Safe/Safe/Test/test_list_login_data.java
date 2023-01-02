f5aed04c89c08a9f9cdf307292719e211cf69fac
ansonc314 <117306795+ansonc314@users.noreply.github.com>
2023-01-02 22:31:28 +0800

    p2v4

--- Project2v4/Safe/Safe/Test/test_list_login_data.java
+++ Project2v4/Safe/Safe/Test/test_list_login_data.java
@@ -4,17 +4,27 @@ import org.junit.Before;
 import org.junit.Test;
 import java.lang.*;
 import java.io.IOException;
+import java.util.ArrayList;
+import javafx.scene.control.ListView;
 
 import static org.junit.Assert.assertEquals;
 
-public class test_data {
+public class test_list_login_data {
     data instruction;
+
     @Before
     public void setup() throws IOException {
     }
 
     @Test
     public void mytest() throws IOException {
+    Main test_main = new Main();
+    //test_main.start();
+
+   //     list_login_data list = new list_login_data("test_data_file.txt");
+        mixing rand = new mixing("password");
+        ArrayList<String> list = new ArrayList<>();
+
         data instruction = new data ("test_data_file.txt");
         instruction.create_line_array();
         int list_size = instruction.lines.size();
@@ -24,4 +34,5 @@ public class test_data {
         assertEquals("line2",str.substring(6,11));
 
     }
+
 }
--- Project2v4/Safe/Safe/src/main/java/com/example/safe/class_trial2.java
+++
@@ -1,6 +0,0 @@
-package com.example.safe;
-
-public class class_trial2 {
-    public int value = 110;
-
-}
