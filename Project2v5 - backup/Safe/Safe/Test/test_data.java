57132a9bcf96704e5bb33b7f5f78a3b97d27271e
ansonc314 <117306795+ansonc314@users.noreply.github.com>
2023-01-19 08:44:49 +0800

    p2v5

---
+++ Project2v5 - backup/Safe/Safe/Test/test_data.java
@@ -0,0 +1,33 @@
+package com.example.safe;
+
+import org.junit.Before;
+import org.junit.Test;
+import java.lang.*;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+public class test_data {
+    data instruction;
+    @Before
+    public void setup() throws IOException {
+    }
+
+    @Test // this test check create_line_array if it can read all lines in the file
+    public void mytest_create_line_array() throws IOException {
+        data instruction = new data ("test_data_file.txt");
+        instruction.create_line_array();
+        int list_size = instruction.lines.size();
+        assertEquals(list_size,2);
+
+
+    }
+
+    @Test // this test check if the read2string function perform correctly or not.
+    public void mytest_read2string() throws IOException {
+        data instruction = new data ("test_data_file.txt");
+        instruction.create_line_array();
+        String str = instruction.read2string();
+        assertEquals("line1\nline2\n",str.substring(0,12));
+    }
+}
