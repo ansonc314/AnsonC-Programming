dc8a53c332a41b30bf4dcfea3a630881ee5261d2
ansonc314 <117306795+ansonc314@users.noreply.github.com>
2023-08-05 15:00:28 +0930

    module 2

--- Anson Programming12/unit2/Assignment2/src/module2_5/Bonus.java
+++ Anson Programming12/unit2/Assignment2/src/module2_5/Bonus.java
@@ -4,24 +4,34 @@ import module2_4.ReadFile;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Bonus {
     public static void main(String[] args) throws IOException {
-        Cipher cipher = new Cipher(0);
-        ArrayList<String> line = ReadFile.Read2StringArray("words_alpha.txt");   // read the file to string array list
-        ArrayList<String> decryptLine = new ArrayList<>();   // declare a string array for storing decrypted words
+        /**
+         * e.g. if the ciphertext is bcepnfo.
+         * then we check if key = 1, the plaintext is abdomen
+         * The word is in the dictionary and so we declare the decryption is successful
+         */
 
-        String filenamePrefix = "trial";  // the decrypted file will be store in a filename "trial" + x where x is the key
-       for (int i=0; i<26; i++){
-           decryptLine.clear();
-           String filename = filenamePrefix + i;
-           cipher.resetKey(i);   // set the key to i
-                for(String s: line){
-                    String decryptString = cipher.decryptString(s);
-                    decryptLine.add(decryptString);
-                }
-            Write2File.write2txt(filename,decryptLine);
-       }
+        ArrayList<String> dictionary = ReadFile.Read2StringArray("words_alpha.txt");   // read the file to string array list
+
+        System.out.println( "Enter the encrypted word\n" );  // Ask for the ciphertext word
+        Scanner scanner = new Scanner( System.in );
+        String cipherWord = scanner.nextLine();
+
+        Cipher cipher = new Cipher(0);   // create the object for decryption
+        for (int i=0; i<26; i++){
+            cipher.resetKey(i);   // set the key to i
+            String decryptString = cipher.decryptString(cipherWord);   // decrypt the word
+            if (dictionary.contains(decryptString)){    // if the decrypted word is in the words_alpha.txt, then we find the decrypted word
+                System.out.println("The key is found and is equal to "  + i);
+                System.out.println("The decrypted word is  "  + cipher.decryptString(cipherWord));
+
+            }
+        }
+
+
     }
 
 }
